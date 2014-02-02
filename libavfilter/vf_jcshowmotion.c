764e9fcbbc884df1381790be82aa6aa92d286b68
Jan <xhpohanka@gmail.com>
2014-02-02 20:20:30 +0100

    jcshowmotion: correct line drawing

--- libavfilter/vf_jcshowmotion.c
+++ libavfilter/vf_jcshowmotion.c
@@ -64,13 +64,13 @@ typedef struct {
     int vsub, hsub;   ///< chroma subsampling
     char *t_expr;          ///< expression for thickness
     int *mip_info;
-} DrawBoxContext;
+} ShowMotionContext;
 
 static const int NUM_EXPR_EVALS = 2;
 
 static av_cold int init(AVFilterContext *ctx)
 {
-    DrawBoxContext *s = ctx->priv;
+    ShowMotionContext *s = ctx->priv;
     uint8_t rgba_color[4];
 
     if (!strcmp(s->color_str, "invert"))
@@ -94,7 +94,7 @@ static av_cold int init(AVFilterContext *ctx)
 
 static av_cold void uninit(AVFilterContext *ctx)
 {
-	DrawBoxContext *s = ctx->priv;
+	ShowMotionContext *s = ctx->priv;
 
 	av_free(s->mip_info);
 }
@@ -116,7 +116,7 @@ static int query_formats(AVFilterContext *ctx)
 static int config_input(AVFilterLink *inlink)
 {
     AVFilterContext *ctx = inlink->dst;
-    DrawBoxContext *s = ctx->priv;
+    ShowMotionContext *s = ctx->priv;
     const AVPixFmtDescriptor *desc = av_pix_fmt_desc_get(inlink->format);
     double var_values[VARS_NB], res;
     char *expr;
@@ -140,6 +140,10 @@ static int config_input(AVFilterLink *inlink)
             goto fail;
         s->thickness = var_values[VAR_T] = res;
     }
+
+    /* we do not want thicker frame than it make sense */
+    if (s->thickness > inlink->w/20/2)
+        s->thickness = inlink->w/20/2;
 
     av_log(ctx, AV_LOG_VERBOSE, "color:0x%02X%02X%02X%02X\n",
            s->yuv_color[Y], s->yuv_color[U], s->yuv_color[V], s->yuv_color[A]);
@@ -182,7 +186,7 @@ static int read_mip_info(char *str, int *mip_info)
 
 static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
 {
-    DrawBoxContext *s = inlink->dst->priv;
+    ShowMotionContext *s = inlink->dst->priv;
     AVDictionaryEntry *e;
     int ret;
     int i;
@@ -203,6 +207,7 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
     for (i = 0; i < 15*20; i++) {
         int plane, x, y, xb, yb, w, h;
         unsigned char *row[4];
+        int drawtop, drawbottom, drawleft, drawright;
 
         if (s->mip_info[i] == 0)
             continue;
@@ -211,6 +216,10 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
         yb = frame->height / 15 * (i / 20);
         w = frame->width/20;
         h = frame->height/15;
+        drawtop = i < 20 || !s->mip_info[i-20];
+        drawbottom = i > 300-20 || !s->mip_info[i+20];
+        drawleft = i%20 == 0 || !s->mip_info[i-1];
+        drawright = i%20 == 19 || !s->mip_info[i+1];
         for (y = FFMAX(yb, 0); y < frame->height && y < (yb + h); y++) {
             row[0] = frame->data[0] + y * frame->linesize[0];
 
@@ -220,16 +229,22 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
 
             if (s->invert_color) {
                 for (x = FFMAX(xb, 0); x < xb + w && x < frame->width; x++)
-                    if ((y - yb < s->thickness) || (yb + h - 1 - y < s->thickness) ||
-                            (x - xb < s->thickness) || (xb + w - 1 - x < s->thickness))
-                        row[0][x] = 0xff - row[0][x];
+                    if (s->thickness >= w/2 ||
+                        (y - yb < s->thickness && drawtop) ||
+                        (yb + h - 1 - y < s->thickness && drawbottom) ||
+                        (x - xb < s->thickness && drawleft) ||
+                        (xb + w - 1 - x < s->thickness && drawright))
+                        row[0][x           ] = 0xff - row[0][x];
             } else {
                 for (x = FFMAX(xb, 0); x < xb + w && x < frame->width; x++) {
                     double alpha = (double)s->yuv_color[A] / 255;
 
-                    if ((y - yb < s->thickness) || (yb + h - 1 - y < s->thickness) ||
-                            (x - xb < s->thickness) || (xb + w - 1 - x < s->thickness)) {
-                        row[0][x                 ] = (1 - alpha) * row[0][x                 ] + alpha * s->yuv_color[Y];
+                    if (s->thickness >= w/2 ||
+                        (y - yb < s->thickness && drawtop) ||
+                        (yb + h - 1 - y < s->thickness  && drawbottom) ||
+                        (x - xb < s->thickness && drawleft) ||
+                        (xb + w - 1 - x < s->thickness && drawright)) {
+                        row[0][x           ] = (1 - alpha) * row[0][x           ] + alpha * s->yuv_color[Y];
                         row[1][x >> s->hsub] = (1 - alpha) * row[1][x >> s->hsub] + alpha * s->yuv_color[U];
                         row[2][x >> s->hsub] = (1 - alpha) * row[2][x >> s->hsub] + alpha * s->yuv_color[V];
                     }
@@ -242,16 +257,16 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
     return ff_filter_frame(inlink->dst->outputs[0], frame);
 }
 
-#define OFFSET(x) offsetof(DrawBoxContext, x)
+#define OFFSET(x) offsetof(ShowMotionContext, x)
 #define FLAGS AV_OPT_FLAG_VIDEO_PARAM|AV_OPT_FLAG_FILTERING_PARAM
 
 #if CONFIG_JCSHOWMOTION_FILTER
 
 static const AVOption jcshowmotion_options[] = {
-    { "color",     "set color of the box",                         OFFSET(color_str), AV_OPT_TYPE_STRING, { .str = "black" }, CHAR_MIN, CHAR_MAX, FLAGS },
-    { "c",         "set color of the box",                         OFFSET(color_str), AV_OPT_TYPE_STRING, { .str = "black" }, CHAR_MIN, CHAR_MAX, FLAGS },
-    { "thickness", "set the box thickness",                        OFFSET(t_expr),    AV_OPT_TYPE_STRING, { .str="3" },       CHAR_MIN, CHAR_MAX, FLAGS },
-    { "t",         "set the box thickness",                        OFFSET(t_expr),    AV_OPT_TYPE_STRING, { .str="3" },       CHAR_MIN, CHAR_MAX, FLAGS },
+    { "color",     "set color of the box",  OFFSET(color_str), AV_OPT_TYPE_STRING, { .str = "black" }, CHAR_MIN, CHAR_MAX, FLAGS },
+    { "c",         "set color of the box",  OFFSET(color_str), AV_OPT_TYPE_STRING, { .str = "black" }, CHAR_MIN, CHAR_MAX, FLAGS },
+    { "thickness", "set the box thickness", OFFSET(t_expr),    AV_OPT_TYPE_STRING, { .str="3" },       CHAR_MIN, CHAR_MAX, FLAGS },
+    { "t",         "set the box thickness", OFFSET(t_expr),    AV_OPT_TYPE_STRING, { .str="3" },       CHAR_MIN, CHAR_MAX, FLAGS },
     { NULL }
 };
 
@@ -279,7 +294,7 @@ static const AVFilterPad jcshowmotion_outputs[] = {
 AVFilter ff_vf_jcshowmotion = {
     .name          = "jcshowmotion",
     .description   = NULL_IF_CONFIG_SMALL("Draw a colored box on the input video."),
-    .priv_size     = sizeof(DrawBoxContext),
+    .priv_size     = sizeof(ShowMotionContext),
     .priv_class    = &jcshowmotion_class,
     .init          = init,
     .uninit		   = uninit,
